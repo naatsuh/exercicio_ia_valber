cfba9bbe800a9a6a9174b2a81d194a909703f99d
naatsuh <38865980+naatsuh@users.noreply.github.com>
2019-09-09 20:21:36 -0300

    Update atividade.pl

--- atividade.pl
+++ atividade.pl
@@ -1,27 +1,42 @@
 %Fatos
-%familia parte de pai
-progenitor(joao, cleber).
-progenitor(joao, isadora).
-progenitor(joao, laercio).
-progenitor(maria, cleber).
-progenitor(maria, isadora).
-progenitor(maria, laercio).
-progenitor(cleber, jonas).
-progenitor(larissa, jonas).
-progenitor(isadora, jose).
-progenitor(marcos, jose).
-progenitor(laercio, laercio_alberto).
-progenitor(laercio, igor).
+%familia_parte_de_pai
+progenitor(joao,cleber).
+progenitor(joao,isadora).
+progenitor(joao,laercio).
+progenitor(maria,cleber).
+progenitor(maria,isadora).
+progenitor(maria,laercio).
+progenitor(cleber,jonas).
+progenitor(larissa,jonas).
+progenitor(isadora,jose).
+progenitor(marcos,jose).
+progenitor(laercio,laercio_alberto).
+progenitor(laercio,igor).
 %-
-progenitor(cassia, bernardo).
-progenitor(cassia, joao_victor).
-progenitor(cassia, emanuel).
-
+progenitor(cassia,bernardo).
+progenitor(cassia,joao_victor).
+progenitor(cassia,emanuel).
+%familia_parte_de_mae
+progenitor(zacarias,wilma).
+progenitor(zacarias,joana).
+progenitor(zacarias,mauro).
+progenitor(carla,wilma).
+progenitor(carla,joana).
+progenitor(carla,mauro).
+progenitor(wilma,laercio_alberto).
+progenitor(wilma,igor).
+progenitor(joana,alice).
+progenitor(lucas,alice).
+progenitor(mauro,isabela).
+progenitor(mauro,lara).
+progenitor(camilla,isabela).
+progenitor(camilla,lara).
 %sexo
-feminino(maria).
-feminino(isadora).
-feminino(larissa).
-
+%parte_mae
+masculino(zacarias).
+masculino(lucas).
+masculino(mauro).
+%parte_de_pai
 masculino(joao).
 masculino(cleber).
 masculino(marcos).
@@ -31,31 +46,11 @@ masculino(jose).
 masculino(igor).
 masculino(laercio_alberto).
 %-
-feminino(cassia).
 masculino(bernardo).
 masculino(joao_victor).
 masculino(emanuel).
-%-------------------------------------
-%familia parte de mae
-progenitor(zacarias, wilma).
-progenitor(zacarias, joana).
-progenitor(zacarias, mauro).
-progenitor(carla, wilma).
-progenitor(carla, joana).
-progenitor(carla, mauro).
-progenitor(wilma, laercio_alberto).
-progenitor(wilma, igor).
-progenitor(joana, alice).
-progenitor(lucas, alice).
-progenitor(mauro, isabela).
-progenitor(mauro, lara).
-progenitor(camilla, isabela).
-progenitor(camilla, lara).
-%sexo
-masculino(zacarias).
-masculino(lucas).
-masculino(mauro).
-
+%-
+%parte de mae
 feminino(carla).
 feminino(wilma).
 feminino(joana).
@@ -63,11 +58,17 @@ feminino(alice).
 feminino(camilla).
 feminino(isabela).
 feminino(lara).
-
-%--------------------------------
+%-
+feminino(cassia).
+%-
+%parte_pai
+feminino(maria).
+feminino(isadora).
+feminino(larissa).
+%-
 %regras
+pai(X,Y):- progenitor(X,Y),masculino(X).
+mae(X,Y):- progenitor(X,Y),feminino(X).
 filhos(Y,X):- progenitor(X,Y).
 filho(Y,X):- progenitor(X,Y),masculino(Y).
 filha(Y,X):- progenitor(X,Y),feminino(Y).
-pai(Y,X):- progenitor(Y,X),masculino(X).
-mae(Y,X):- progenitor(Y,X),feminino(X).
